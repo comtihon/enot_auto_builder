1599751e4545a4819d709b28b025b966d328ffa0
Ekaterina Belova <ek.d.belova@gmail.com>
2017-12-01 20:03:39 +0100

    frontend added, partly working

---
+++ src/main/resources/static/js/src/search.js
@@ -0,0 +1,78 @@
+
+//class to represent erlang package row in searсh package grid
+function ErlPackage(build_id, namespace, name, success, build_date, path, version, erl_version) {
+    var self = this;
+    self.build_id = ko.observable(build_id);
+    self.namespace = ko.observable(namespace);
+    self.name = ko.observable(name);
+    self.success = ko.observable(success);
+    self.build_date = ko.observable(build_date);
+    self.path = ko.observable(path);
+    self.version = ko.observable(version);
+    self.erl_version = ko.observable(erl_version);
+}
+
+function PackageViewModel() {
+    var self = this;
+    self.justTestText = "im here";
+    console.log(self.justTestText);
+    self.searchDone = ko.observable(true);
+    console.log(self.searchDone);
+
+    self.packages = ko.observableArray([]);
+    self.err_msg = function(msg) {
+        alert(msg);
+    }
+    self.toggleTableVisible = function() {
+    }
+    
+    self.simpleSearch = function(input_name) {
+        $.ajax({
+          url: "/packages",
+          type: "get", //send it through get method
+          data: {
+            name: input_name.trim()
+          },
+          dataType: 'json',
+          success: function(data) {
+              if (!data[0].result) {
+                ko.mapping.fromJS(data[0].response, self.packages);
+              } else {
+                self.err_msg(data[0].response);
+              }
+          },
+          error: function(xhr) {
+            self.err_msg(xhr);
+            //Do Something better to handle error
+          }
+        });
+    }
+    self.complexSearch = function(namespace, name, version, erl_version) {
+//            FIXME code duplication, extract base method
+            $.ajax({
+              url: "/packages",
+              type: "get", //send it through get method
+              data: {
+                name: name.trim(),
+                namespace: namespace.trim(),
+                version: version.trim(),
+                erl_version: erl_version.trim()
+              },
+              dataType: 'json',
+              success: function(data) {
+                  if (!data[0].result) {
+                    ko.mapping.fromJS(data[0].response, self.packages);
+                  } else {
+                    self.err_msg(data[0].response);
+                  }
+              },
+              error: function(xhr) {
+                self.err_msg(xhr);
+                //Do Something better to handle error
+              }
+            });
+    }
+}
+
+// Activates knockout.js
+ko.applyBindings(new PackageViewModel());
